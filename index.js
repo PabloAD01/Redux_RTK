1166ed823aecf066878610bfcdc0ae7adffe6d0e
Pablo Araya Diaz <piarayad@gmail.com>
2023-09-10 21:54:19 -0300

    feat: add IceCream Reducer

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
+const icecreamActions = require("./features/icecream/icecreamSlice").icecreamActions;
 
-console.log("initial state", store.getState());
+console.log("Initial state", store.getState());
 const unsusbscribe = store.subscribe(() => {
-  console.log("state changed", store.getState());
+  console.log("State changed", store.getState());
 });
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.restocked(3));
+
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.ordered());
+store.dispatch(icecreamActions.restocked(3));
+
+
 unsusbscribe();
