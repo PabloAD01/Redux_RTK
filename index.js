7a6e8c65e64d29e1d713fe6dd885d19b032aadea
Pablo Araya Diaz <piarayad@gmail.com>
2023-09-10 22:28:16 -0300

    feat: add Extra Reducers

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const icecreamActions = require("./features/icecream/icecreamSlice").icecreamActions;
 
 console.log("Initial state", store.getState());
-const unsusbscribe = store.subscribe(() => {});
+const unsusbscribe = store.subscribe(() => {
+  console.log("Updated state", store.getState());
+});
 
 store.dispatch(cakeActions.ordered());
 store.dispatch(cakeActions.ordered());
